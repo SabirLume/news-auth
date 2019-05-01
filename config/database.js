35972297fe812137771f7b97ac259723b6bb8623
SabirLume <sabirlume@gmail.com>
2019-05-01 11:37:43 -0400

    checking for update

--- config/database.js
+++ config/database.js
@@ -4,3 +4,4 @@ module.exports = {
     'url' : `mongodb+srv://sabir:${process.env.MONGOPASSWORD}@crud-1-x5qne.mongodb.net/news?retryWrites=true`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': 'news'
 };
+console.log("updated pw", process.env.MONGOPASSWORD )
