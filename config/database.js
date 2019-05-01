061d805d574159458b147550e2ed80dfabf8e473
SabirLume <sabirlume@gmail.com>
2019-05-01 11:32:52 -0400

    hiding pw

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://sabir:sabir@crud-1-x5qne.mongodb.net/news?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : `mongodb+srv://sabir:${process.env.MONGOPASSWORD}@crud-1-x5qne.mongodb.net/news?retryWrites=true`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': 'news'
 };
